7ff7a779064308ac4ea0738cbc6c9e30d9299141
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2022-04-19 09:46:28 +0200

    fixes for JDK 18

--- foreign-linker/src/main/java/pl/symentis/jvm/foreign/linker/GetPIDExample.java
+++ foreign-linker/src/main/java/pl/symentis/jvm/foreign/linker/GetPIDExample.java
@@ -2,24 +2,25 @@ package pl.symentis.jvm.foreign.linker;
 
 import jdk.incubator.foreign.CLinker;
 import jdk.incubator.foreign.FunctionDescriptor;
+import jdk.incubator.foreign.ValueLayout;
 
 import java.lang.invoke.MethodType;
 
-public class GetPIDExample {
+public class GetPIDExample
+{
 
-    public static void main(String[] args) throws Throwable {
-        var libraryLookup = CLinker.systemLookup();
+    public static void main( String[] args ) throws Throwable
+    {
+        var cLinker = CLinker.systemCLinker();
 
-        var getpidSymbol = libraryLookup.lookup("getpid")
-                .orElseThrow(() -> new RuntimeException("getpid symbol not found"));
+        var getpidSymbol = cLinker.lookup( "getpid" )
+                                  .orElseThrow( () -> new RuntimeException( "getpid symbol not found" ) );
 
-        var functionDescriptor = FunctionDescriptor.of(CLinker.C_INT);
-        var methodType = MethodType.methodType(int.class);
-        var methodHandle = CLinker.getInstance()
-                .downcallHandle(getpidSymbol,
-                        methodType,
-                        functionDescriptor);
-        System.out.println(methodHandle.invoke());
+        var functionDescriptor = FunctionDescriptor.of( ValueLayout.JAVA_INT );
+        var methodHandle = cLinker
+                .downcallHandle( getpidSymbol,
+                                 functionDescriptor );
+        System.out.println( methodHandle.invoke() );
     }
 }
 
--- foreign-linker/src/main/java/pl/symentis/jvm/foreign/linker/SIGTERM.java
+++ foreign-linker/src/main/java/pl/symentis/jvm/foreign/linker/SIGTERM.java
@@ -4,42 +4,43 @@ import jdk.incubator.foreign.CLinker;
 import jdk.incubator.foreign.FunctionDescriptor;
 import jdk.incubator.foreign.MemoryAddress;
 import jdk.incubator.foreign.ResourceScope;
+import jdk.incubator.foreign.ValueLayout;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
 import static java.lang.System.out;
 
-public class SIGTERM {
+public class SIGTERM
+{
 
-    public static void main(String[] args) throws Throwable {
-        var symbolLookup = CLinker.systemLookup();
+    public static void main( String[] args ) throws Throwable
+    {
+        var cLinker = CLinker.systemCLinker();
 
-        var cLinker = CLinker.getInstance();
-
-        out.println("creating signal handler stub");
+        out.println( "creating signal handler stub" );
         // QUESTION: why the hell global scope?
         var resourceScope = ResourceScope.globalScope();
         var signalHandler = MethodHandles.lookup()
-                .findStatic(SIGTERM.class, "onSignal",
-                        MethodType.methodType(void.class, int.class));
-        var signalHandlerStub = cLinker.upcallStub(signalHandler,
-                FunctionDescriptor.ofVoid(CLinker.C_INT), resourceScope);
+                                         .findStatic( SIGTERM.class, "onSignal",
+                                                      MethodType.methodType( void.class, int.class ) );
+        var signalHandlerStub = cLinker.upcallStub( signalHandler,
+                                                    FunctionDescriptor.ofVoid( ValueLayout.JAVA_INT ), resourceScope );
 
-        out.println("installing signal handler " + signalHandlerStub);
-        var signal = symbolLookup.lookup("signal")
-                .orElseThrow(() -> new RuntimeException("signal symbol not found"));
-        var signalHandle = cLinker.downcallHandle(signal,
-                MethodType.methodType(void.class, int.class, MemoryAddress.class),
-                FunctionDescriptor.ofVoid(CLinker.C_INT, CLinker.C_POINTER));
-        out.println(signalHandle.type());
-        signalHandle.invoke(15, signalHandlerStub.address());
+        out.println( "installing signal handler " + signalHandlerStub );
+        var signal = cLinker.lookup( "signal" )
+                            .orElseThrow( () -> new RuntimeException( "signal symbol not found" ) );
+        var signalHandle = cLinker.downcallHandle( signal,
+                                                   FunctionDescriptor.ofVoid( ValueLayout.JAVA_INT, ValueLayout.ADDRESS ) );
+        out.println( signalHandle.type() );
+        signalHandle.invoke( 15, signalHandlerStub.address() );
 
-        out.println("Press <ENTER> to quit");
+        out.println( "Press <ENTER> to quit" );
         System.in.read();
     }
 
-    public static void onSignal(int signal) {
-        out.println("Received signal " + signal);
+    public static void onSignal( int signal )
+    {
+        out.println( "Received signal " + signal );
     }
 }
